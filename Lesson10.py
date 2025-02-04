a632605e4e62ea25110c71b90ce4320921c043d6
EugenSiva <eugensiva@gmail.com>
2025-02-04 18:56:43 +0100

    Lesson10

--- Lesson10.py
+++ Lesson10.py
@@ -1,1 +1,11 @@
-print("Ahoj")
+class Vehicle:
+    def __init__(self, vehicle_id, vehicle_name, vehicle_type):
+        self.vehicle_id = vehicle_id
+        self.vehicle_name = vehicle_name
+        self.vehicle_type = vehicle_type
+
+    def vehicle(self):
+        print(f"Moje vozidlo ma SPZ: {self.vehicle_id}, jeho meno je {self.vehicle_name} a je to {self.vehicle_type}.")
+
+vozidlo = Vehicle(vehicle_id="BL652GB",vehicle_name="Afto",vehicle_type="Skoda")
+vozidlo.vehicle()
