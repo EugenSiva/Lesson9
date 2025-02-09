fc51030f0a22bea3f2794c64cc97c287d404c404
EugenSiva <eugensiva@gmail.com>
2025-02-09 19:02:45 +0100

    Polymorfizmus v platbach

--- Lesson10/Lesson103.py
+++ Lesson10/Lesson103.py
@@ -1,16 +1,30 @@
 class Platba:
-    def zaplat(self):
+    def zaplat(self, suma):
         raise NotImplementedError("Subclasses must implement the zaplat method")
 
 class Kreditna_Karta(Platba):
-    def zaplat(self):
-        return print("Zaplatil si 50 kreditnou kartou.")
+    def zaplat(self, suma):
+        print(f"Zaplatil si {suma} eur kreditnou kartou.")
 
 class PayPal(Platba):
-    def zaplat(self):
-        return print("Zaplatil si 50 cez PayPal.")
+    def zaplat(self, suma):
+        print(f"Zaplatil si {suma} eur cez PayPal.")
 
 
 class Hotovost(Platba):
-    def zaplat(self):
-        return print("Zaplatil si 50 v hotovosti.")
+    def zaplat(self, suma):
+        print(f"Zaplatil si {suma} v hotovosti.")
+
+
+if __name__ == "__main__":
+    suma = 50
+
+    kreditna_karta = Kreditna_Karta()
+    kreditna_karta.zaplat(suma)
+
+    paypal = PayPal()
+    paypal.zaplat(suma)
+
+    hotovost = Hotovost()
+    hotovost.zaplat(suma)
+
