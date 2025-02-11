453142b8e316505e3f3e831f81c7b942fbbdbd99
EugenSiva <eugensiva5@gmail.com>
2025-02-11 18:41:37 +0100

    static metody

---
+++ Lesson11.py
@@ -0,0 +1,15 @@
+class UnitConverter:
+    @staticmethod
+    def kilometers_to_miles(kilometers):
+        return kilometers * 0.621371
+
+    @staticmethod
+    def miles_to_kilometers(miles):
+        return miles / 0.621371
+
+# Example usage:
+if __name__ == "__main__":
+
+    print(f"5 kilometers is {UnitConverter.kilometers_to_miles(5):.2f} miles")
+    print(f"3 miles is {UnitConverter.miles_to_kilometers(3):.2f} kilometers")
+
